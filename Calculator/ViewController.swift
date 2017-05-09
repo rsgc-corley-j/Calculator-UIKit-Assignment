a1cf7ad698be6ba5ba9a74ecd8e4fb6ec0703022
Student <student@macbook-pro-1174-corley.local>
2017-05-09 17:35:09 -0400

    Still working on plus minus issue, also working to fix percentage

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,26 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
+    
     //outlet to display(label)
     @IBOutlet weak var LabelDisplay: UILabel!
     //create an instance (object) of the calculator class
     var model = Calculator()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
-      //  clear the contents of the label
+        //  clear the contents of the label
         LabelDisplay.text = ""
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func onePressed(_ sender: Any) {
         model.addToNewValue(digit: "1")
         LabelDisplay.text = model.providedValue
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
         model.addToNewValue(digit: "2")
         LabelDisplay.text = model.providedValue
     }
-
+    
     @IBAction func threePressed(_ sender: Any) {
         model.addToNewValue(digit: "3")
         LabelDisplay.text = model.providedValue
@@ -76,9 +76,9 @@ class ViewController: UIViewController {
         model.addToNewValue(digit: "0")
         LabelDisplay.text = model.providedValue
     }
-   
+    
     @IBAction func multiply(_ sender: Any) {
-      model.multiply()
+        model.multiply()
     }
     
     @IBAction func divide(_ sender: Any) {
@@ -98,21 +98,32 @@ class ViewController: UIViewController {
         LabelDisplay.text = model.providedValue
     }
     @IBAction func plusMinus(_ sender: Any) {
-    model.plusMinus()
-    if model.providedValue == ""{
-         LabelDisplay.text = String(describing: model.computedValue!)
-    }else{
-    LabelDisplay.text = model.providedValue
-    }
+        //    model.plusMinus()
+        
+        if model.computedValue == nil{
+            
+        }else{
+            model.plusMinus()
+            if model.providedValue == ""{
+                LabelDisplay.text = String(describing: model.computedValue!)
+            }else{
+                LabelDisplay.text = model.providedValue
+            }
+        }
     }
     
     @IBAction func percentage(_ sender: Any) {
-    model.percentage()
+        
+        if model.computedValue == nil{
+            
+        }else{
+            model.percentage()
+        }
     }
     
     @IBAction func decimal(_ sender: Any) {
-    model.decimal()
-    LabelDisplay.text = model.providedValue
+        model.decimal()
+        LabelDisplay.text = model.providedValue
     }
     
     @IBAction func equals(_ sender: Any) {
